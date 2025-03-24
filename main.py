25b7a74e15813c35a153094e7f9bd9bc0b0a6fcd
Srujan B <srujanb28@gmail.com>
2025-03-24 00:14:57 +0000

    changes in main

--- main.py
+++ main.py
@@ -11,11 +11,25 @@ if __name__ == "__main__":
 
         service = VisionModelServices()
 
-        image_path = "/root/ChatBotLLM/Scenary.jpg"
+        image_path = "/root/ChatBotLLM/acne.jpg"
 
         with open(image_path,"rb") as img_file:
 
-            prompt = "Describe the image"
+            prompt = """
+You are a professional dermatologist assistant. Analyze the given image and describe any visible skin condition or abnormality. Based on the appearance, categorize the condition as either minor or serious.
+
+If the condition appears minor, provide practical suggestions, tips, or over-the-counter remedies that can help manage or improve the condition.
+
+If the condition appears serious (e.g., severe infection, suspicious moles, deep wounds, signs of skin cancer, or anything that requires medical attention), respond strictly with:
+'This condition appears serious. Please consult a qualified dermatologist or healthcare professional immediately.'
+
+Do not attempt to give home remedies or solutions for serious cases.
+
+and at the end add "Disclaimer: This analysis is AI-generated and should not replace professional medical advice. 
+
+
+and add in bold "do you still need a consult with General Practioner ?"
+"""
 
             output = service.process_request(image_file=image_path, promt= prompt)
 
